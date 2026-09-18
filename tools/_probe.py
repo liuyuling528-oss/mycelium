6798df2249ade2b53cee02f35cd2c32193a769c9
yuling <liuyuling528@gmail.com>
2026-09-18 15:43:27 +0800

    清理误提交的 Chrome 测试 profile；.gitignore 挡住 tools/_* 临时产物

--- tools/_probe.py
+++
@@ -1,6 +0,0 @@
-﻿import urllib.request
-try:
-    r = urllib.request.urlopen("http://127.0.0.1:8491/index.html", timeout=5)
-    print("8491 alive, status", r.status)
-except Exception as e:
-    print("8491 dead:", e)
